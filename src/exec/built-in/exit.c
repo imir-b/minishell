bdcca6a7025a5edc2396fbe2e4cb43a35ce1c448
Easy-doro <vladimir.bleskine@gmail.com>
2026-03-28 16:55:17 +0100

    Feat: add basic built in for env command and pwd cmd

--- src/exec/built-in/exit.c
+++ src/exec/built-in/exit.c
@@ -3,11 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vbleskin <vbleskin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vlad <vlad@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 03:47:53 by vlad              #+#    #+#             */
-/*   Updated: 2026/03/26 16:07:37 by vbleskin         ###   ########.fr       */
+/*   Updated: 2026/03/28 16:54:29 by vlad             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	ft_exit(int status)
+{
+	/* if (ac > 2)
+		exit: too many arguments
+	if (!(isdigit(status)))
+		exit: status: numeric argument -> exit(255)
+	ft_putstr_fd("exit\n", STDOUT_FILENO);
+	exit(status);
+	*/
+}
--- src/exec/built-in/pwd.c
+++ src/exec/built-in/pwd.c
@@ -3,13 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   pwd.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vbleskin <vbleskin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vlad <vlad@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/20 03:47:12 by vlad              #+#    #+#             */
-/*   Updated: 2026/03/26 16:05:45 by vbleskin         ###   ########.fr       */
+/*   Updated: 2026/03/28 16:49:01 by vlad             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//donne la position actuelle 
+int	ft_pwd(void)
+{
+	size_t	size;
+	char	*ret;
+	char	*buffer;
+
+	size = 1024;
+	buffer = malloc(sizeof(char) * size);
+	if (!buffer)
+		return (1);
+	ret = getcwd(buffer, size);
+	while (ret == NULL)
+	{
+		if (errno == ERANGE)
+		{
+			free(buffer);
+			size *= 2;
+			buffer = malloc(sizeof(char) * size);
+			if (!buffer)
+				return (1);
+			ret = getcwd(buffer, size);
+		}
+		else
+			return (free(buffer), 1);
+	}
+	ft_putstr_fd(buffer, STDOUT_FILENO);
+	ft_putchar_fd('\n', STDOUT_FILENO);
+	free(buffer);
+	return (0);
+}
